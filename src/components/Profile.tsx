f164ca34337636e106bc63282603e4474fd4199c
有働龍太 <ryutaudo@gmail.com>
2022-01-16 22:06:06 +0900

    Add social link to profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
-import s from './Profile.module.css'
+import Link from 'next/link'
+import s from './Profile.module.scss'
 
 export default function Profile() {
   return (
@@ -17,6 +18,18 @@ export default function Profile() {
         <p className={s.desc2}>
           プログラミングから日常のことまで幅広く書いていくブログです。
         </p>
+        <div className={s.social}>
+          <Link href="https://twitter.com/ryuta_udo">
+            <a>
+              <Image src="/images/twitter.svg" width={30} height={30} />
+            </a>
+          </Link>
+          <Link href="https://github.com/ryutaudo">
+            <a>
+              <Image src="/images/github.svg" width={30} height={30} />
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
   )
